98377a2502b1651982d7cc289b42797d2ee93cac
roboeast <roboeast@gmail.com>
2022-01-05 18:00:03 -0500

    Brocolli and cheese is yummy.

--- Scenes/Brocolli.gd
+++ Scenes/Brocolli.gd
@@ -1,12 +1,22 @@
 extends Area2D
 
 
+onready var animation = $AnimationPlayer as AnimationPlayer
 
 
-func _on_Brocolli_body_entered(body):
-	$AnimationPlayer.play("bounce")
+func _ready() -> void:
+	# Manually connect the signal so we don't get warnings if we comment out the function
+	animation.connect("animation_finished", self, "_on_AnimationPlayer_animation_finished")
 
 
+# Play a bounce animation
+func _on_Brocolli_body_entered(_body: Node) -> void:
+	# The bounce animation must be local to the node otherwise it will bounce near 0,0 of the level (top left)
+	# I changed it to bounce the sprite position not the parent node position (Brocolli)
+	animation.play("bounce")
 
-#func _on_AnimationPlayer_animation_finished(anim_name):
-#	queue_free()
+
+# Delete the brocolli
+func _on_AnimationPlayer_animation_finished(_anim_name: String) -> void:
+	queue_free()
+
